fc2b998a66218dfe4565e6c8972dfcd9fbc6810b
王喻杰 <11706987+asdnasduanda@user.noreply.gitee.com>
2024-12-22 11:23:15 +0800

    删除菜品业务代码-代码优化

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -12,6 +12,5 @@ public interface DishFlavorMapper {
     void insertBatch(List<DishFlavor> flavors);
     @Delete("delete from dish_flavor where dish_id = #{dishId}")
     void deleByDishId(Long dishId);
-
     void deleByDishIds(List<Long> dishIds);
 }
